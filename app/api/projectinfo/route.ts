e3b4fc30bcbf9197b054e21a6f87a657b48b574d
PBS-Admin <pbsadmin@pbsbuildings.com>
2026-07-21 09:02:44 -0700

    Turn on try catch for dates

--- app/api/projectinfo/route.ts
+++ app/api/projectinfo/route.ts
@@ -234,7 +234,7 @@ export async function POST(request: NextRequest) {
         columns.push({ name: "OnHold", value: 1 });
     }
 
-//    try {
+    try {
       const formattedDateSubmitted = validateAndFormatDatetime(
         DateSubmitted,
         "DateSubmitted",
@@ -251,12 +251,11 @@ export async function POST(request: NextRequest) {
         EstDateComp,
         "EstDateComp",
       );
-//      if (formattedEstDateComp !== null) {
-//        columns.push({ name: "EstDateComp", value: formattedEstDateComp });
-//      } else {
-//        columns.push({ name: "EstDateComp", value: null });
-//      }
-        columns.push({ name: "EstDateComp", value: EstDateComp });
+      if (formattedEstDateComp !== null) {
+        columns.push({ name: "EstDateComp", value: formattedEstDateComp });
+      } else {
+        columns.push({ name: "EstDateComp", value: null });
+      }
 
       const formattedBidDate = validateAndFormatDatetime(BidDate, "BidDate");
       if (formattedBidDate !== null) {
@@ -265,9 +264,9 @@ export async function POST(request: NextRequest) {
         columns.push({ name: "BidDate", value: null });
       }
 
-//    } catch (err: any) {
-//      return failResponse("Invalid DATA1", err.message);
-//    }
+    } catch (err: any) {
+      return failResponse("Invalid DATA1", err.message);
+    }
 
     // Check if AdjutantID already exists
     const [existing] = await toolsPool.query<IToolQuote[]>(
